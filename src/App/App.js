c142cab350e41ff1197c5af6bc0cea0c48164b3b
MCrank <marco_mcse@hotmail.com>
2019-01-07 18:38:49 -0600

    Setup Complete

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header" />
+        <Button color="primary">primary</Button>{' '}
+      </div>
+    );
+  }
+}
+
+export default App;
