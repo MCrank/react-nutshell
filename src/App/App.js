a858b024fa5c386a4ca344aaebb1776cdf257569
MCrank <marco_mcse@hotmail.com>
2019-01-08 20:39:38 -0600

    Added navbar and routes for pages

--- src/App/App.js
+++ src/App/App.js
@@ -33,6 +33,7 @@ const PrivateRoute = ({ component: Component, authed, ...rest }) => {
 class App extends React.Component {
   state = {
     authed: false,
+    pendingUser: true,
   };
 
   componentDidMount() {
@@ -42,10 +43,12 @@ class App extends React.Component {
       if (user) {
         this.setState({
           authed: true,
+          pendingUser: false,
         });
       } else {
         this.setState({
           authed: false,
+          pendingUser: false,
         });
       }
     });
@@ -55,13 +58,8 @@ class App extends React.Component {
     this.removeListener();
   }
 
-  // isAuthenticated = () => {
-  //   this.setState({
-  //     authed: true,
-  //   });
-  // };
-
   render() {
+    const { authed, pendingUser } = this.state;
     const logoutClickEvent = () => {
       authRequests.logoutUser();
       this.setState({
@@ -69,30 +67,28 @@ class App extends React.Component {
       });
     };
 
-    // if (!this.state.authed) {
-    //   return (
-    //     <div className="App">
-    //       <MyNavbar isAuthed={this.state.authed} logoutClickEvent={logoutClickEvent} />
-    //       <Auth isAuthenticated={this.isAuthenticated} />
-    //     </div>
-    //   );
-    // }
+    if (pendingUser) {
+      return null;
+    }
+
     return (
       <div className="App">
         <BrowserRouter>
           <React.Fragment>
             <MyNavbar isAuthed={this.state.authed} logoutClickEvent={logoutClickEvent} />
-            <div className="row">
-              <Switch>
-                <PrivateRoute path="/" exact component={Home} authed={this.state.authed} />
-                <PrivateRoute path="/home" component={Home} authed={this.state.authed} />
-                <PublicRoute path="/auth" component={Auth} authed={this.state.authed} />
-                <PrivateRoute path="/friends" component={Friends} authed={this.state.authed} />
-                <PrivateRoute path="/articles" component={Articles} authed={this.state.authed} />
-                <PrivateRoute path="/weather" component={Weather} authed={this.state.authed} />
-                <PrivateRoute path="/events" component={Events} authed={this.state.authed} />
-                <PrivateRoute path="/messages" component={Messages} authed={this.state.authed} />
-              </Switch>
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <PrivateRoute path="/" exact component={Home} authed={authed} />
+                  <PrivateRoute path="/home" component={Home} authed={authed} />
+                  <PublicRoute path="/auth" component={Auth} authed={authed} />
+                  <PrivateRoute path="/friends" component={Friends} authed={authed} />
+                  <PrivateRoute path="/articles" component={Articles} authed={authed} />
+                  <PrivateRoute path="/weather" component={Weather} authed={authed} />
+                  <PrivateRoute path="/events" component={Events} authed={authed} />
+                  <PrivateRoute path="/messages" component={Messages} authed={authed} />
+                </Switch>
+              </div>
             </div>
           </React.Fragment>
         </BrowserRouter>
--- src/components/MyNavbar/MyNavbar.js
+++ src/components/MyNavbar/MyNavbar.js
@@ -18,15 +18,56 @@ class MyNavbar extends React.Component {
 
   render() {
     const { isAuthed, logoutClickEvent } = this.props;
+    const buildNavbar = () => {
+      if (isAuthed) {
+        return (
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink tag={RRNavLink} to="/friends">
+                Friends
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={RRNavLink} to="/articles">
+                Articles
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={RRNavLink} to="/weather">
+                Weather
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={RRNavLink} to="/events">
+                Events
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={RRNavLink} to="/messages">
+                Messages
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={RRNavLink} onClick={logoutClickEvent} to="/home">
+                Logout
+              </NavLink>
+            </NavItem>
+          </Nav>
+        );
+      }
+      return <Nav className="ml-auto" navbar />;
+    };
+
     return (
       <div className="MyNavbar">
         <Navbar color="dark" dark expand="md" fixed={'top'}>
           <NavbarBrand href="/">React Nutshell</NavbarBrand>
           <NavbarToggler onClick={e => this.toggle(e)} />
           <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
+            {buildNavbar()}
+            {/* <Nav className="ml-auto" navbar>
               <NavItem>{isAuthed ? <NavLink onClick={logoutClickEvent}>Logout</NavLink> : ''}</NavItem>
-            </Nav>
+            </Nav> */}
           </Collapse>
         </Navbar>
       </div>
