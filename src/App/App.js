fe0d0abbd9e1fd655511c3b38b7defe062a80170
MCrank <marco_mcse@hotmail.com>
2019-01-08 18:56:58 -0600

    Public Route

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import {
+  BrowserRouter, Route, Redirect, Switch,
+} from 'react-router-dom';
 
 import connection from '../helpers/data/connection';
-import Auth from '../components/Auth/Auth';
+import Auth from '../components/pages/Auth/Auth';
 
 import './App.scss';
 import MyNavbar from '../components/MyNavbar/MyNavbar';
 import authRequests from '../helpers/data/authRequests';
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  // props contains Location, Match, and History
+  const routeChecker = props => (authed === false ? <Component {...props} /> : <Redirect to={{ pathname: '/home', state: { from: props.location } }} />);
+  return <Route {...rest} render={props => routeChecker(props)} />;
+};
 
 class App extends React.Component {
   state = {
@@ -45,19 +54,26 @@ class App extends React.Component {
       });
     };
 
-    if (!this.state.authed) {
-      return (
-        <div className="App">
-          <MyNavbar isAuthed={this.state.authed} logoutClickEvent={logoutClickEvent} />
-          <Auth isAuthenticated={this.isAuthenticated} />
-        </div>
-      );
-    }
+    // if (!this.state.authed) {
+    //   return (
+    //     <div className="App">
+    //       <MyNavbar isAuthed={this.state.authed} logoutClickEvent={logoutClickEvent} />
+    //       <Auth isAuthenticated={this.isAuthenticated} />
+    //     </div>
+    //   );
+    // }
     return (
       <div className="App">
-        <MyNavbar isAuthed={this.state.authed} logoutClickEvent={logoutClickEvent} />
-        <h3 className="mt-5">You are Authenticated</h3>
-        <header className="App-header" />
+        <BrowserRouter>
+          <React.Fragment>
+            <MyNavbar isAuthed={this.state.authed} logoutClickEvent={logoutClickEvent} />
+            <div className="row">
+              <Switch>
+                <PublicRoute path="/auth" component={Auth} authed={this.state.authed} isAuthenticated={this.isAuthenticated} />
+              </Switch>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
       </div>
     );
   }
