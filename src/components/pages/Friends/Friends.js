477cfde20b304c393833182e314c520d14162d6a
MCrank <marco_mcse@hotmail.com>
2019-01-12 17:34:44 -0600

    Added users state object, populate users with data from firebase

--- src/components/pages/Friends/Friends.js
+++ src/components/pages/Friends/Friends.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import smashRequests from '../../../helpers/data/smashRequests';
+import authRequests from '../../../helpers/data/authRequests';
+
 import './Friends.scss';
 
 class Friends extends React.Component {
+  state = {
+    users: [],
+  };
+
+  componentDidMount() {
+    smashRequests
+      .usersAndFriends(authRequests.getCurrentUid())
+      .then((users) => {
+        this.setState({ users });
+      })
+      .catch(error => console.error('stuff broke', error));
+  }
+
   render() {
     return (
       <div className="Friends">
